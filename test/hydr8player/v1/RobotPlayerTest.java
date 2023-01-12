46175c293ca3f1d756cba281ae4bc0a0b955dd39
Your Name <you@example.com>
2023-01-11 21:26:38 -0500

    test(init): create test package structure

---
+++ test/hydr8player/v1/RobotPlayerTest.java
@@ -0,0 +1,13 @@
+package hydr8player.v1;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class RobotPlayerTest {
+
+    @Test
+    public void testSanity() {
+        assertEquals(2, 1+1);
+    }
+
+}
