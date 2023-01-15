8c1bde2de2eac810af23f4b7c1f69908c92de404
Michael Klapper <mklapp2@wgu.edu>
2023-01-15 14:45:50 -0800

    Adds and fixes Pathfinding_V01 functions. Adds: getClockwiseMoveDirectionsN0() - returns array of directions excluding Center with index 0 = North and the rest in clockwise order from North. getClosestValidMoveDirection() - returns the valid movement direction with least offset from a target direction. getRotateValidMove() - returns the first valid move proceeding clockwise, starting at a target direction.

--- src/omega_scrimbot_1/Pathfinding/Pathfinding_V01.java
+++ src/omega_scrimbot_1/Pathfinding/Pathfinding_V01.java
@@ -2,12 +2,25 @@ package omega_scrimbot_1.Pathfinding;
 import battlecode.common.*;
 
 public class Pathfinding_V01 {
+    /**
+     * Returns list of all non-Center directions in clockwise order with North at index 0.
+     * @return list of all non-Center directions in clockwise order with North at index 0.
+     */
+    public static Direction[] getClockwiseMoveDirectionsN0(){
+        Direction[] goodDirections = new Direction[] {Direction.NORTH, Direction.NORTHEAST,
+                Direction.EAST, Direction.SOUTHEAST, Direction.SOUTH, Direction.SOUTHWEST, Direction.WEST,
+                Direction.NORTHWEST};
+        return goodDirections;
+    }
+
     // find closest movable direction to desired direction
-    static Direction getClosestValidMove(RobotController rc, Direction desired_dir){
+    public static Direction getClosestValidMoveDirection(RobotController rc, Direction desired_dir){
         if(rc.canMove(desired_dir)) return desired_dir;
-        for (int rotation_offset = 1; rotation_offset <= 4; rotation_offset++){  // 4 is 1/2 of the 8 possible directions
-            Direction left_dir = Direction.values()[(desired_dir.ordinal() +  rotation_offset) % 8];
-            Direction right_dir = Direction.values()[(desired_dir.ordinal() + 8 - rotation_offset) % 8];
+        for (int rotation_offset = 1; rotation_offset <= 4; rotation_offset++){  // 4 is 1/2 of the 8 possible movement
+            // directions
+            Direction[] moveDirections = getClockwiseMoveDirectionsN0();
+            Direction left_dir = moveDirections[(desired_dir.ordinal() + 8 + rotation_offset) % 8];
+            Direction right_dir = moveDirections[(desired_dir.ordinal() + 8 - rotation_offset) % 8];
             if (rc.canMove(left_dir)) return left_dir;
             if (rc.canMove(right_dir)) return right_dir;
         }
@@ -28,18 +41,18 @@ public class Pathfinding_V01 {
 
     /**
      * Returns the next valid move direction starting with the startDirection and rotating in the selected direction
-     * until a valid move is found.
+     * until a valid move is found. If no valid move is found, returns Direction.Center.
      * @param rc The robot controller.
      * @param startDirection The direction to start with/from.
      * @param rotateClockwise If true, will try clockwise. Else anti-clockwise.
      * @return Next valid move in the desired rotation direction, or Center direction if no other valid move found.
      */
-    static Direction getRotateValidMove(RobotController rc, Direction startDirection, boolean rotateClockwise) {
+    public static Direction getRotateValidMove(RobotController rc, Direction startDirection, boolean rotateClockwise) {
         if (rc.canMove(startDirection)){
             return startDirection;
         }
         else {
-            Direction[] directions = Direction.allDirections();
+            Direction[] directions = getClockwiseMoveDirectionsN0();
             int attempts = 1;
             int incrementVal = 1;
 
@@ -49,8 +62,9 @@ public class Pathfinding_V01 {
 
             int curOffset = incrementVal;
             while (attempts < directions.length){
-                Direction proposedDir = directions[curOffset % directions.length];
-                if ((proposedDir.ordinal() != 0) && rc.canMove(proposedDir)){
+                Direction proposedDir = directions[(startDirection.ordinal() + curOffset + directions.length)
+                        % directions.length];
+                if (rc.canMove(proposedDir)){
                     return proposedDir;
                 }
                 else {
