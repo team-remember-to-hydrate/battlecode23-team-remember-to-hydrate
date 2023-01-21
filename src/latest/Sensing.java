0f6f980131cdc7a520c3b72d77912b753f1f04ff
Michael Klapper <mklapp2@wgu.edu>
2023-01-21 11:01:26 -0800

    Adds scanEconEnemies, scanEnemyAnchorCarrier

--- src/latest/Sensing.java
+++ src/latest/Sensing.java
@@ -26,7 +26,6 @@ public class Sensing {
     }
 
     // Scan all nearby enemies
-
     /**
      * Scans for all enemies in vision radius, using less bytecode if a scan already occurred this turn (retains most
      * recent scan).
@@ -44,7 +43,6 @@ public class Sensing {
 
 
     // Filter to nearby combat enemies
-
     /**
      * Smart scans for all enemies in vision range and returns those that are Launchers or Destabilizers.
      * @param rc
@@ -56,7 +54,7 @@ public class Sensing {
         ArrayList<RobotInfo> enemyCombatRobots = new ArrayList<RobotInfo>();
         int enemyRobotsLength = enemyRobots.length; // saves bytecode
         for (int i = 0; i < enemyRobotsLength; i++){
-            //RobotInfo targetBot = enemyRobots[i];
+            //RobotInfo targetBot = enemyRobots[i]; // TODO: Check if bytecode decreases by alternate implementation.
             if (enemyRobots[i].getType() == RobotType.LAUNCHER || enemyRobots[i].getType() == RobotType.DESTABILIZER){
                 enemyCombatRobots.add(enemyRobots[i]);
             }
@@ -64,20 +62,70 @@ public class Sensing {
         return enemyCombatRobots;
     }
 
-    // Return weakest Robot from set
-
     // Filter to nearby econ enemies
+    public static ArrayList<RobotInfo> scanEconEnemies(RobotController rc) throws GameActionException {
+        RobotInfo[] enemyRobots = smartScanEnemies(rc);
+        ArrayList<RobotInfo> enemyEconRobots = new ArrayList<RobotInfo>();
+        int enemyRobotsLength = enemyRobots.length; // saves bytecode
+        for (int i = 0; i < enemyRobotsLength; i++){
+            //RobotInfo targetBot = enemyRobots[i]; // TODO: Check if bytecode decreases by alternate implementation.
+            if (enemyRobots[i].getType() == RobotType.CARRIER || enemyRobots[i].getType() == RobotType.BOOSTER){ //||
+                    //enemyRobots[i].getType() == RobotType.HEADQUARTERS){ // Removing this to prevent accidental targeting of HQs.
+                enemyEconRobots.add(enemyRobots[i]);
+            }
+        }
+        return enemyEconRobots;
+    }
 
-    // Sense enemy Carriers with Anchors
+    // Sense single enemy Carrier with Anchor
+    public static RobotInfo scanEnemyAnchorCarrier(RobotController rc) throws GameActionException {
+        RobotInfo[] enemyRobots = smartScanEnemies(rc);
+        int enemyRobotsLength = enemyRobots.length; // saves bytecode
+        for (int i = 0; i < enemyRobotsLength; i++){
+            if (enemyRobots[i].getType() == RobotType.CARRIER && enemyRobots[i].getTotalAnchors() > 0){ //||
+                return enemyRobots[i];
+            }
+        }
+        return null;
+    }
 
-    // Scan all nearby wells
 
-    // ____________Below this are things that won't change during a game________
-    // Only need to sense each of these once (per bot). Skip sensing these if you sensed last turn and didn't move.
+    // Return weakest Robot from set
+    public static RobotInfo scanWeakestBotInGroup(RobotController rc, RobotInfo[] botGroup) {
+        int lengthBotGroup = botGroup.length;
+        int lowestHealthIndex = 0;
+        int lowestHealthSoFar = botGroup[lowestHealthIndex].getHealth();
+        for (int i = 0; i < lengthBotGroup; i++){
+            int botHealth = botGroup[i].getHealth();
+            if (botHealth < lowestHealthSoFar) {
+                lowestHealthSoFar = botHealth;
+                lowestHealthIndex = i;
+            }
+        }
+        return botGroup[lowestHealthIndex];
+    }
+
+
+
 
     // Scan all newly visible island squares
     // Decided to abandon this, as at a vision range of 20 it would take over 200 bytecode to senseIsland on each newly
     //  visible square.
+
+    //
+    public static short senseBitpackedIslandLocation(RobotController rc, MapLocation targetLocation){
+        return -1;
+    }
+
+    public static short senseBitpackedIslandDetails(RobotController rc, MapLocation targetLocation){
+        return -1;
+    }
+
+
+    // ____________Below this are things that won't change during a game________
+    // Only need to sense each of these once (per bot). Skip sensing these if you sensed last turn and didn't move.
+
+
 
     // Scan all nearby walls
     public static ArrayList<MapLocation> scanNearbyWalls(RobotController rc) throws GameActionException {
