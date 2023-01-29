56ab45834e686de942b71857f171377fbdda7706
Michael Klapper <mklapp2@wgu.edu>
2023-01-28 19:46:50 -0800

    Fixes bad island Comms behavior due to is_location() output being inverted

--- src/latest/Comms.java
+++ src/latest/Comms.java
@@ -67,24 +67,24 @@ public class Comms {
         return countPartitions.length - 1;
     }
 
-    static void getCommsUpdates(RobotController rc) throws GameActionException {
-        getIslandUpdates(rc);
-    }
-
-    private static void getIslandUpdates(RobotController rc) throws GameActionException {
-        for (int i = index_island; i < index_last_island + 1; i++){
-            int commArrayValue = rc.readSharedArray(i);
-            int detailValue = 0;
-            if (commArrayValue != 0){
-                if (is_location(commArrayValue)){
-                    // Get next comm array value
-                    i++;
-                    detailValue = rc.readSharedArray(i);
-
-                }
-            }
-        }
-    }
+//    static void getCommsUpdates(RobotController rc) throws GameActionException {
+//        getIslandUpdates(rc);
+//    }
+//
+//    private static void getIslandUpdates(RobotController rc) throws GameActionException {
+//        for (int i = index_island; i < index_last_island + 1; i++){
+//            int commArrayValue = rc.readSharedArray(i);
+//            int detailValue = 0;
+//            if (commArrayValue != 0){
+//                if (is_location(commArrayValue)){
+//                    // Get next comm array value
+//                    i++;
+//                    detailValue = rc.readSharedArray(i);
+//
+//                }
+//            }
+//        }
+//    }
 
 
     static int decompressCountToMinPossible(int compressedCount){
@@ -225,7 +225,7 @@ public class Comms {
     }
 
     static boolean is_location(int array_data){
-        return (array_data & 0b1000000000000000) == 0;
+        return (array_data & 0b1000000000000000) == 1;
     }
 
     static int get_island_id(int array_data) throws GameActionException{
